73315f9214dfce94615ecf46cdc1e0d10d8f3bda
covaleva.ian100 <covaleva.ian100@gmail.com>
2019-11-06 21:29:38 +0200

    second commit

--- index.php
+++ index.php
@@ -3,15 +3,6 @@
   <meta charset="utf-8">
   <title>output</title>
 
-<style>
-   body {
-    background: url(images/sun2.png) no-repeat;
-    -moz-background-size: 100%; /* Firefox 3.6+ */
-    -webkit-background-size: 100%; /* Safari 3.1+ и Chrome 4.0+ */
-    -o-background-size: 100%; /* Opera 9.6+ */
-    background-size: 100%; /* Современные браузеры */
-   }
-  </style>
 
 <style>
   .header-h1 {
